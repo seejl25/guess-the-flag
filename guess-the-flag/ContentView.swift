bba73e2bf2fe04aa3218f48870e70b7d33123778
seejl25 <seejunlong@u.nus.edu>
2025-08-02 14:36:55 +0800

    Initial Commit

---
+++ guess-the-flag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  guess-the-flag
+//
+//  Created by See Jun Long on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ guess-the-flagTests/guess_the_flagTests.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flagTests.swift
+//  guess-the-flagTests
+//
+//  Created by See Jun Long on 2/8/25.
+//
+
+import Testing
+@testable import guess_the_flag
+
+struct guess_the_flagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
